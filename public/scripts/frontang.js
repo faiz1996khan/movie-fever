42bfeba8f11a4b0531172f75561eb6d72c22a5df
Faiz Khan <38392769+faiz1996khan@users.noreply.github.com>
2020-11-04 20:31:28 +0530

    Update frontang.js

--- public/scripts/frontang.js
+++ public/scripts/frontang.js
@@ -27,12 +27,12 @@ const app = angular.module('myApp',['ngRoute']);
 
 app.controller('myCntr',['$scope','$http','$location', '$anchorScroll','$window',function($scope,$http,$location,$anchorScroll,$window){
     $scope.getmovie = function(title){
-        $http.get('https://api.themoviedb.org/3/search/movie?api_key=1248d09d0d407f99a897a41fb634e6b3&query='+title+'&language=en-US&include_adult=true').then(function(list){
+        $http.get('https://api.themoviedb.org/3/search/movie?api_key={your_api_key}&query='+title+'&language=en-US&include_adult=true').then(function(list){
             $scope.all = list.data.results;
         })
     }
     $scope.goto = function(title){
-        $http.get('https://api.themoviedb.org/3/search/movie?api_key=1248d09d0d407f99a897a41fb634e6b3&query='+title).then(function(title){
+        $http.get('https://api.themoviedb.org/3/search/movie?api_key={your_api_key}&query='+title).then(function(title){
             $window.location.href = '#!/details/'+title.data.results[0].id;
         })
         $scope.all = null;
@@ -44,7 +44,7 @@ app.controller('myCntr',['$scope','$http','$location', '$anchorScroll','$window'
         return true;
     }
         $scope.movies_info = [];
-            $http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&page=1').then(function(data){
+            $http.get('https://api.themoviedb.org/3/movie/now_playing?api_key={your_api_key}&language=en-US&page=1').then(function(data){
                 let res = data.data.results;
                 let chunk = 4;
                 let local = [];
@@ -72,7 +72,7 @@ app.controller('myCntr',['$scope','$http','$location', '$anchorScroll','$window'
 
 app.controller('upCntr',['$scope','$http','$location', '$anchorScroll',function($scope,$http,$location,$anchorScroll){
     $scope.movies_info = [];
-        $http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&page=1').then(function(data){
+        $http.get('https://api.themoviedb.org/3/movie/upcoming?api_key={your_api_key}&language=en-US&page=1').then(function(data){
             let res = data.data.results;
             let chunk = 4;
             let local = [];
@@ -100,7 +100,7 @@ app.controller('upCntr',['$scope','$http','$location', '$anchorScroll',function(
 }]);
 
 app.controller('tv_detailCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US').then(function(tv_details){
+    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'?api_key={your_api_key}&language=en-US').then(function(tv_details){
         $scope.details_tv = tv_details.data;
         $scope.seasons = tv_details.data.seasons.slice(1,tv_details.data.seasons.length);
         console.log($scope.seasons);
@@ -108,19 +108,19 @@ app.controller('tv_detailCntr',['$scope','$http','$routeParams',function($scope,
 }])
 
 app.controller('tv_galleryCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/videos?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US').then(function(vid_details){
+    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/videos?api_key={your_api_key}&language=en-US').then(function(vid_details){
         $scope.tv_key = vid_details.data.results[1].key;
     })
 }])
 
 app.controller('tv_detCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/reviews?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&page=1').then(function(review){
+    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/reviews?api_key={your_api_key}&language=en-US&page=1').then(function(review){
         $scope.tv_review = review.data.results;
     })
 }])
 
 app.controller('detailCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&adult=true').then(function(res){
+    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'?api_key={your_api_key}&language=en-US&adult=true').then(function(res){
         $scope.details = res.data;
         $http.get('http://www.omdbapi.com/?apikey=b14679c7&i='+$scope.details.imdb_id).then(function(res){
         $scope.moreInfo = res.data;
@@ -130,17 +130,17 @@ app.controller('detailCntr',['$scope','$http','$routeParams',function($scope,$ht
 }])
 
 app.controller('galleryCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/videos?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US').then(function(videos){
+    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/videos?api_key={your_api_key}&language=en-US').then(function(videos){
         $scope.video = videos.data.results;
         $scope.url= $scope.video[0].key;
     })
-    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/images?api_key=1248d09d0d407f99a897a41fb634e6b3').then(function(images){
+    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/images?api_key={your_api_key}').then(function(images){
         $scope.img = images.data.backdrops;
     })
 }])
 
 app.controller('detCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
-    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/reviews?api_key=1248d09d0d407f99a897a41fb634e6b3').then(function(ans){
+    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/reviews?api_key={your_api_key}').then(function(ans){
         $scope.review = ans.data.results;
             $scope.arr = [];
             for(let i=0 ; i<$scope.review.length ; i++){
@@ -155,7 +155,7 @@ app.controller('detCntr',['$scope','$http','$routeParams',function($scope,$http,
 
 app.controller('similarCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
     $scope.similarMovies_info = [];
-    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/similar?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&page=1').then(function(mov){
+    $http.get('https://api.themoviedb.org/3/movie/'+$routeParams.id+'/similar?api_key={your_api_key}&language=en-US&page=1').then(function(mov){
         let movie = mov.data.results;
         let chunk = 4;
         let local = [];
@@ -176,7 +176,7 @@ app.controller('similarCntr',['$scope','$http','$routeParams',function($scope,$h
 
 app.controller('tv_similarCntr',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
     $scope.tv_similarMovies_info = [];
-    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/similar?api_key=1248d09d0d407f99a897a41fb634e6b3&language=en-US&page=1').then(function(mov){
+    $http.get('https://api.themoviedb.org/3/tv/'+$routeParams.id+'/similar?api_key={your_api_key}&language=en-US&page=1').then(function(mov){
         let movie = mov.data.results;
         let chunk = 4;
         let local = [];
@@ -202,13 +202,13 @@ app.controller('advanceCntr',['$scope','$http','$sortMovies',function($scope,$ht
     $scope.now = d.getFullYear();
     $scope.adv_search = function(term,genres,year,sort_by){
         if(sort_by === 'Movie'){
-        $http.get('https://api.themoviedb.org/3/search/movie?api_key=1248d09d0d407f99a897a41fb634e6b3&query='+term+'&language=en-US&include_adult=true').then(function(list){
+        $http.get('https://api.themoviedb.org/3/search/movie?api_key={your_api_key}&query='+term+'&language=en-US&include_adult=true').then(function(list){
             let terms = list.data.results;
             $scope.ans_res = $sortMovies.Ans(terms,genres,year);
             $scope.url_path = 'details';
         })
     }else if(sort_by === 'TV'){
-        $http.get('https://api.themoviedb.org/3/search/tv?api_key=1248d09d0d407f99a897a41fb634e6b3&query='+term+'&language=en-US&page=1').then(function(shows){
+        $http.get('https://api.themoviedb.org/3/search/tv?api_key={your_api_key}&query='+term+'&language=en-US&page=1').then(function(shows){
             let terms = shows.data.results;
             let limit = [];
             $scope.url_path = 'tv_details';
